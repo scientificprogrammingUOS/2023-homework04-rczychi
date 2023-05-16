6994492359d776e8c5945569724d16695dd03830
rczychi <106338351+rczychi@users.noreply.github.com>
2023-05-16 23:24:03 +0200

    strange_pattern.py

--- strange_pattern.py
+++ strange_pattern.py
@@ -2,9 +2,15 @@ import numpy as np
 
 # implement the function strange pattern
 
-def strange_pattern():
+def strange_pattern(n):
     # delete the NotImplementedError when you write your function.
-    raise NotImplementedError
+    arr = np.zeros(n, dtype='bool')
+    
+    arr[::3, ::3] = True
+    arr[1::3, 2::3] = True
+    arr[2::3, 1::3] = True
+
+    return arr
 
 
 if __name__ == "__main__":
